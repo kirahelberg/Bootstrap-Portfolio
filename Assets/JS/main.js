2b67eb3e61e94947b7e83b9374458c170addd3b6
Kira Helberg <kira.helberg@gmail.com>
2019-03-17 23:08:33 -0400

    compiled in one html file, added all github links

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -5,3 +5,37 @@ function showNavMenu() {
     document.getElementById("navMenu").style.display = "none";
   }
 }
+
+window.onload = function() {
+  document.getElementById("about-container").style.display = "block";
+};
+
+function showAbout() {
+  if (document.getElementById("about-container").style.display === "none") {
+    document.getElementById("about-container").style.display = "block";
+    document.getElementById("contact-container").style.display = "none";
+    document.getElementById("portfolio-container").style.display = "none";
+  } else {
+    document.getElementById("about-container").style.display = "none";
+  }
+}
+
+function showContact() {
+  if (document.getElementById("contact-container").style.display === "none") {
+    document.getElementById("contact-container").style.display = "block";
+    document.getElementById("about-container").style.display = "none";
+    document.getElementById("portfolio-container").style.display = "none";
+  } else {
+    document.getElementById("contact-container").style.display = "none";
+  }
+}
+
+function showPortfolio() {
+  if (document.getElementById("portfolio-container").style.display === "none") {
+    document.getElementById("portfolio-container").style.display = "block";
+    document.getElementById("about-container").style.display = "none";
+    document.getElementById("contact-container").style.display = "none";
+  } else {
+    document.getElementById("portfolio-container").style.display = "none";
+  }
+}
